a9e885b50dcb8d9de73a7e0dba94387f189d1b46
Nizar <nizar@exequt.com>
2022-10-07 14:11:52 -0400

    yes

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -21,7 +21,7 @@ if(item.id === todo.id){
    
 return(
     <div className="todo">
-        <li className={`todo-item  ${todo.completed ? "completed" : ""}`}>{text}</li>
+        <li >{text}</li>
         <button onClick={completeHandler} className="complete-btn"><i className="fas fa-check"></i></button>
         <button onClick={deleteHandler} className="trash-btn"><i className="fas fa-trash"></i></button>
     </div>
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,19 +1,19 @@
 import React from "react";
 import Todo from "./Todo";
 
-const TodoList = ({todos, setTodos}) => {
+const TodoList = ({ todos, setTodos }) => {
 
-return(
-    <div className="todo-container">
-    <ul className="todo-list">
-      {todos.map((todo) => (
-    <Todo setTodos={setTodos} todos={todos} key ={todo.id} text={todo.text} todo={todo}  />
+  return (
+    <div >
+      <ul >
+        {todos.map((todo) => (
+          <Todo setTodos={setTodos} todos={todos} key={todo.id} text={todo.text} todo={todo} />
 
-      ))}
-    </ul>
-  </div>
+        ))}
+      </ul>
+    </div>
 
-)
+  )
 
 
 }
